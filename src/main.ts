bda6b4843a603f83c64ad34ac69f914c142ce54b
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-18 21:27:22 +0900

    firebase の初期化を行うよう修正

--- src/main.ts
+++ src/main.ts
@@ -1,13 +1,26 @@
 import Vue from 'vue';
+import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
-import Antd from 'ant-design-vue';
+import Antd, { Form } from 'ant-design-vue';
 import 'ant-design-vue/dist/antd.css';
+
+import firebaseAuth from '@/plugins/firebase/auth';
 
 Vue.config.productionTip = false;
 
 Vue.use(Antd);
+
+firebase.initializeApp({
+  apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
+  authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
+  projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
+  storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
+  messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+});
+firebaseAuth.init();
 
 new Vue({
   router,
