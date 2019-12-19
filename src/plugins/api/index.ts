8de03f0cccbe08ae8ccf74d741dd6cb3c48a6a8f
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-19 13:04:59 +0900

    user の store 実装完了により、その store を axios に反映

--- src/plugins/api/index.ts
+++ src/plugins/api/index.ts
@@ -1,31 +1,29 @@
 import axios from 'axios';
-import * as Vuex from 'vuex';
-
-const store: Vuex.ExStore = require('@/store');
+import store from '@/store';
 
 const http = axios.create({
-    baseURL: process.env.VUE_APP_API_BASE_URL
+  baseURL: process.env.VUE_APP_API_BASE_URL
 });
 
 /* TODO: ユーザの store 作成後、以下の↓処理を完成させる */
-// http.interceptors.request.use(
-//     requestConfig => {
-//         const token: string =
-//             store.getters[`${config.vuex.namespace.auths}/token`];
-//         if (token) {
-//             requestConfig.headers.Authorization = `Bearer ${token}`;
-//             return requestConfig;
-//         }
-//         return requestConfig;
-//     },
-//     error => {
-//         return Promise.reject(error);
-//     }
-// );
+http.interceptors.request.use(
+  async requestConfig => {
+    const user: firebase.User | null = store.getters['user/currentUser'];
+
+    if (user) {
+      const token = await user.getIdToken(true);
+      requestConfig.headers.Authorization = `Bearer ${token}`;
+      return requestConfig;
+    }
+    return requestConfig;
+  },
+  error => {
+    return Promise.reject(error);
+  }
+);
 
 export default async (option: any) => {
-    return http(option)
-        .catch((error: any) => {
-            throw error;
-        });
+  return http(option).catch((error: any) => {
+    throw error;
+  });
 };
