1dac95f83cfce90681b565cd2f857948c1ce5cff
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-19 13:17:08 +0900

    lint を実行。インデントの幅を半角4に修正

--- .eslintrc.js
+++ .eslintrc.js
@@ -1,31 +1,32 @@
 module.exports = {
-  root: true,
-  env: {
-    node: true
-  },
-  extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
-  rules: {
-    'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
-    'prettier/prettier': [
-      'error',
-      {
-        singleQuote: true
-      }
+    root: true,
+    env: {
+        node: true
+    },
+    extends: ['plugin:vue/essential', '@vue/prettier', '@vue/typescript'],
+    rules: {
+        'no-console': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'no-debugger': process.env.NODE_ENV === 'production' ? 'error' : 'off',
+        'prettier/prettier': [
+            'error',
+            {
+                singleQuote: true,
+                tabWidth: 4
+            }
+        ]
+    },
+    parserOptions: {
+        parser: '@typescript-eslint/parser'
+    },
+    overrides: [
+        {
+            files: [
+                '**/__tests__/*.{j,t}s?(x)',
+                '**/tests/unit/**/*.spec.{j,t}s?(x)'
+            ],
+            env: {
+                jest: true
+            }
+        }
     ]
-  },
-  parserOptions: {
-    parser: '@typescript-eslint/parser'
-  },
-  overrides: [
-    {
-      files: [
-        '**/__tests__/*.{j,t}s?(x)',
-        '**/tests/unit/**/*.spec.{j,t}s?(x)'
-      ],
-      env: {
-        jest: true
-      }
-    }
-  ]
 };
