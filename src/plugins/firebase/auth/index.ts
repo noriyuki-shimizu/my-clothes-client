e378b6217f2709e5230d65845abba3fe754f6b43
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-19 13:07:08 +0900

    firebase authentication の機能を実装

--- src/plugins/firebase/auth/index.ts
+++ src/plugins/firebase/auth/index.ts
@@ -1,72 +1,55 @@
 import firebase from 'firebase/app';
 import 'firebase/auth';
 
+import {
+  FirebaseExternalApiAuthError,
+  FirebaseNormalAuthError
+} from '@/plugins/firebase/auth/type';
+
+export const isFirebaseExternalApiAuthError = (
+  err: any
+): err is FirebaseExternalApiAuthError => {
+  return 'credential' in err;
+};
+
+export const isFirebaseNormalAuthError = (
+  err: any
+): err is FirebaseNormalAuthError => {
+  return 'code' in err;
+};
+
 export default {
-    init(): void {
-        firebase.auth().setPersistence(firebase.auth.Auth.Persistence.SESSION);
-    },
+  init(): void {
+    firebase.auth().setPersistence(firebase.auth.Auth.Persistence.LOCAL);
+  },
 
-    async createUserWithEmailAndPassword(
-        username: string,
-        password: string
-    ): Promise<firebase.auth.UserCredential> {
-        return firebase
-            .auth()
-            .createUserWithEmailAndPassword(username, password);
-    },
+  async createUserWithEmailAndPassword(
+    mailAddress: string,
+    password: string
+  ): Promise<firebase.auth.UserCredential> {
+    return firebase
+      .auth()
+      .createUserWithEmailAndPassword(mailAddress, password);
+  },
 
-    async signInWithEmailAndPassword(
-        username: string,
-        password: string
-    ): Promise<firebase.auth.UserCredential> {
-        return firebase
-            .auth()
-            .signInWithEmailAndPassword(username, password);
-    },
+  async signInWithEmailAndPassword(
+    mailAddress: string,
+    password: string
+  ): Promise<firebase.auth.UserCredential> {
+    return firebase.auth().signInWithEmailAndPassword(mailAddress, password);
+  },
 
-    async loginWithGoogle(): Promise<void> {
-        const provider = new firebase.auth.GoogleAuthProvider();
-        await firebase.auth().signInWithPopup(provider);
-    },
+  async signInWithGoogle(): Promise<firebase.auth.UserCredential> {
+    const provider = new firebase.auth.GoogleAuthProvider();
+    return firebase.auth().signInWithPopup(provider);
+  },
 
-    async loginWithGithub(): Promise<void> {
-        const provider = new firebase.auth.GithubAuthProvider();
-        await firebase.auth().signInWithPopup(provider);
-    },
+  async signInWithGithub(): Promise<firebase.auth.UserCredential> {
+    const provider = new firebase.auth.GithubAuthProvider();
+    return firebase.auth().signInWithPopup(provider);
+  },
 
-    async logout(): Promise<void> {
-        return firebase.auth().signOut();
-    },
-
-    async onAuth(): Promise<void> {
-        const user = firebase.auth().currentUser;
-
-        const { authState, userStatus, token } = user
-            ? {
-                  authState: user,
-                  userStatus: !!user.uid,
-                  token: await user.getIdToken(true)
-              }
-            : {
-                  authState: {} as firebase.User,
-                  userStatus: false,
-                  token: ''
-              };
-
-        console.info('login user : ', authState);
-
-        /* 以下の処理は actions でやって */
-        // store.commit(
-        //     `${config.vuex.namespace.auths}/onAuthStateChanged`,
-        //     authState
-        // );
-        // store.commit(
-        //     `${config.vuex.namespace.auths}/onUserStatusChanged`,
-        //     userStatus
-        // );
-        // store.commit(
-        //     `${config.vuex.namespace.auths}/onTokenStateChanged`,
-        //     token
-        // );
-    }
+  async logout(): Promise<void> {
+    await firebase.auth().signOut();
+  }
 };
