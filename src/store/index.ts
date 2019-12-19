d01f46cdef92325f9834204a96604a57764929a5
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-19 13:08:17 +0900

    user の store を反映

--- src/store/index.ts
+++ src/store/index.ts
@@ -1,11 +1,13 @@
 import Vue from 'vue';
 import Vuex from 'vuex';
 import counter from '@/store/counter';
+import user from '@/store/user';
 
 Vue.use(Vuex);
 
 export default new Vuex.Store({
   modules: {
-    counter
+    counter,
+    user
   }
 });
