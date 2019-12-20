2028c60e61db1e3baaa4cee1820fc1a9ed33469d
清水紀行 <noriyuki.shimizu0612@gmail.com>
2019-12-20 22:05:44 +0900

    メイン画面の共通部分である view を作成

---
+++ src/views/Common.vue
@@ -0,0 +1,12 @@
+<template>
+    <div id="common">
+        <router-view />
+    </div>
+</template>
+
+<script lang="ts">
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class Common extends Vue {}
+</script>
