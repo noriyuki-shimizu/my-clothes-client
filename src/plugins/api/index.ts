4fff245f244ce9b7eda4d51bab9202e8abeb817b
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-03 21:07:08 +0900

    処理を整理

--- src/plugins/api/index.ts
+++ src/plugins/api/index.ts
@@ -1,32 +1,27 @@
 import axios, { AxiosRequestConfig, AxiosError } from 'axios';
+
 import store from '@/store';
 
 export const isAxiosError = (err: any): err is AxiosError => {
     return 'isAxiosError' in err;
 };
 
-const http = axios.create({
-    baseURL: process.env.VUE_APP_API_BASE_URL
-});
-
-/* TODO: ユーザの store 作成後、以下の↓処理を完成させる */
-http.interceptors.request.use(
-    async requestConfig => {
-        const user: firebase.User | null = store.getters['user/currentUser'];
-
-        if (user) {
-            const token = await user.getIdToken(true);
-            requestConfig.headers.Authorization = `Bearer ${token}`;
-            return requestConfig;
-        }
-        return requestConfig;
-    },
-    error => {
-        return Promise.reject(error);
-    }
-);
-
 export default async (option: AxiosRequestConfig) => {
+    const http = axios.create({
+        baseURL: process.env.VUE_APP_API_BASE_URL
+    });
+
+    http.interceptors.request.use(
+        async requestConfig => {
+            const accessToken = store.getters['user/accessToken'];
+            requestConfig.headers.Authorization = `Bearer ${accessToken}`;
+            return requestConfig;
+        },
+        error => {
+            return Promise.reject(error);
+        }
+    );
+
     return http(option).catch((error: any) => {
         throw error;
     });
