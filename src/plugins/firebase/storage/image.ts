19363f08aafd2291f5d355fbb0fbf06c8fa90455
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-05 00:17:04 +0900

    ファイル名を変更

---
+++ src/plugins/firebase/storage/image.ts
@@ -0,0 +1,60 @@
+import * as firebase from 'firebase/app';
+import * as url from 'url';
+
+const getImageRef = (name: string, subFolder: string) => {
+    const storageRef = firebase.storage().ref();
+
+    const IMAGE_FOLDER_NAME = `images/${subFolder}`;
+    return storageRef.child(IMAGE_FOLDER_NAME + name);
+};
+
+const upload = async (
+    file: File | undefined,
+    subFolder: string = ''
+): Promise<string> => {
+    if (!file) {
+        throw new Error('Necessary information is missing!!');
+    }
+
+    const imageRef = getImageRef(file.name, subFolder);
+
+    const snapshot = await imageRef.put(file);
+
+    return snapshot.ref.getDownloadURL();
+};
+
+const deleteImageByName = async (
+    name: string,
+    subFolder: string = ''
+): Promise<void> => {
+    const imageRef = getImageRef(name, subFolder);
+    await imageRef.delete().catch((error: any) => {
+        throw error;
+    });
+};
+
+const deleteImageByFullPath = async (fullPath: string): Promise<void> => {
+    const decodeURL = decodeURIComponent(fullPath);
+    const imageRef = firebase.storage().refFromURL(decodeURL);
+
+    const parseURL = url.parse(decodeURL);
+    const { pathname } = parseURL as { pathname: string };
+    const extension = pathname.split('.').pop();
+    const imageRef200 = firebase
+        .storage()
+        .refFromURL(
+            decodeURL.replace(`.${extension}`, `_200x200.${extension}`)
+        );
+
+    await Promise.all([imageRef.delete(), imageRef200.delete()]).catch(
+        (error: any) => {
+            throw error;
+        }
+    );
+};
+
+export default {
+    upload,
+    deleteImageByName,
+    deleteImageByFullPath
+};
