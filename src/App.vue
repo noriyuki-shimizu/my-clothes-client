93f0fae5458d637dc300bd8cd6d4f3d0d0c27db2
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-04 23:45:29 +0900

    class-component に変更

--- src/App.vue
+++ src/App.vue
@@ -3,9 +3,9 @@
         <router-view />
     </div>
 </template>
+<script>
+import { Vue, Component } from 'vue-property-decorator';
 
-<script lang="ts">
-import { Vue } from 'vue-property-decorator';
-
+@Component
 export default class App extends Vue {}
 </script>
