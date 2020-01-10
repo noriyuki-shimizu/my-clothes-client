03cdc5a5b6aa8aa58130e9818bc122475b9345a0
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-10 14:23:17 +0900

    バリデーションチェックを削除

--- src/components/brand/form.ts
+++ src/components/brand/form.ts
@@ -1,50 +1,5 @@
-import { FieldDecorator, Form } from 'ant-design-vue/types/form/form';
+import { Form } from 'ant-design-vue/types/form/form';
 import { DoneUploadFileInfo } from 'ant-design-vue/types/upload';
-
-export const decorator: Readonly<FieldDecorator> = {
-    name: [
-        'name',
-        {
-            rules: [
-                {
-                    max: 60,
-                    message: 'Name cannot be longer than 60 characters!'
-                },
-                {
-                    required: true,
-                    message: 'Please input your name!'
-                }
-            ]
-        }
-    ],
-    image: ['image', {}],
-    link: [
-        'link',
-        {
-            rules: [
-                {
-                    max: 100,
-                    message: 'Link cannot be longer than 100 characters!'
-                }
-            ]
-        }
-    ],
-    country: [
-        'country',
-        {
-            rules: [
-                {
-                    max: 15,
-                    message: 'Country cannot be longer than 15 characters!'
-                },
-                {
-                    required: true,
-                    message: 'Please input your country!'
-                }
-            ]
-        }
-    ]
-};
 
 export const formItemLayout: Readonly<ExPartial<Form>> = {
     labelCol: { span: 6 },
