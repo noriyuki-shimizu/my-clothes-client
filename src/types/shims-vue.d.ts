498e14fba01c6325b5ecd6fa980296e07716b93d
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-01-18 19:05:44 +0900

    $emit の型を拡張

--- src/types/shims-vue.d.ts
+++ src/types/shims-vue.d.ts
@@ -1,4 +1,9 @@
-declare module '*.vue' {
-    import Vue from 'vue';
-    export default Vue;
+import Vue from 'vue';
+
+declare module 'vue/types/vue' {
+    interface Vue {
+        // TODO: 一旦の対応として、ジェネリクスで退避
+        // 統一感を持たせるのなら、vuex と同じように型を定義する
+        $emit<T>(event: string, t: T): this;
+    }
 }
