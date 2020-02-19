59d0b084b749e84f080888acdd75226d7827f077
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-02-19 22:52:19 +0900

    firebase の 初期化を別ファイルに抽出

--- src/main.ts
+++ src/main.ts
@@ -1,23 +1,16 @@
 import Vue from 'vue';
-import firebase from 'firebase';
 import App from './App.vue';
 import router from './router';
 import store from './store';
 
 import setAntdComponents from '@/plugins/antd/components';
+import firebaseApp from '@/plugins/firebase/app';
 import firebaseAuth from '@/plugins/firebase/auth';
 
 setAntdComponents(Vue);
 Vue.config.productionTip = false;
 
-firebase.initializeApp({
-    apiKey: process.env.VUE_APP_FIREBASE_API_KEY,
-    authDomain: process.env.VUE_APP_FIREBASE_AUTH_DOMAIN,
-    databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
-    projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
-    storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-    messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
-});
+firebaseApp.init();
 firebaseAuth.init();
 
 new Vue({
