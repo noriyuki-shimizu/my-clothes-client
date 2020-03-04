ec7537c7c45ae98755ed8d669c4265214efc944b
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-04 22:58:34 +0900

    form 用の table をコンポーネント化

---
+++ src/components/clothes/FormTable.vue
@@ -0,0 +1,93 @@
+<template>
+    <a-table
+        class="select-item-table"
+        :dataSource="tableItems"
+        :columns="columns"
+        :scroll="{ x: 1200, y: 500 }"
+        :pagination="{ pageSize: 50 }"
+        :loading="tableLoading"
+        :rowSelection="{
+            selectedRowKeys,
+            onSelect
+        }"
+    >
+        <span slot="genres" slot-scope="genres">
+            <a-tag v-for="(genre, i) in genres" :color="genre.color" :key="i">
+                {{ genre.name }}
+            </a-tag>
+        </span>
+
+        <span slot="price" slot-scope="price">
+            <div class="column-price">
+                {{ price.toLocaleString() }}
+            </div>
+        </span>
+
+        <span slot="satisfaction" slot-scope="satisfaction">
+            <a-rate
+                :defaultValue="satisfaction"
+                disabled
+                allow-half
+                style="text-align: center !important"
+            />
+        </span>
+
+        <a-icon
+            slot="filterIcon"
+            slot-scope="filtered"
+            type="search"
+            :style="{ color: filtered ? '#108ee9' : undefined }"
+        />
+
+        <span slot="imageLink" slot-scope="imageLink">
+            <img
+                class="coordinate-item-table-image"
+                :src="imageLink ? imageLink : require('@/assets/no-img.png')"
+            />
+        </span>
+    </a-table>
+</template>
+
+<script lang="ts">
+import { Vue, Component, Prop } from 'vue-property-decorator';
+import * as Vuex from 'vuex';
+
+import { Record } from './type';
+import { getColumns } from './table';
+
+@Component
+export default class ClothesFormTable extends Vue {
+    @Prop({ type: Array as () => Record[] })
+    tableItems!: Record[];
+
+    @Prop({ type: Array as () => number[], default: [] })
+    selectedRowKeys?: number[];
+
+    columns = getColumns(
+        this.$store.getters['clothes/assistBrands'],
+        this.$store.getters['clothes/assistShops'],
+        this.$store.getters['clothes/assistGenres']
+    );
+
+    tableLoading = false;
+
+    async created() {
+        this.tableLoading = true;
+
+        await Promise.all([
+            this.$store.dispatch('coordinate/fetchCoordinateItems'),
+            this.$store.dispatch('clothes/fetchAssistGenres'),
+            this.$store.dispatch('clothes/fetchAssistBrands'),
+            this.$store.dispatch('clothes/fetchAssistShops')
+        ]).catch(err => {
+            this.$emit('on-error', err);
+        });
+
+        this.tableLoading = false;
+    }
+
+    onSelect(record: Record) {
+        this.$emit('on-select', record.key);
+    }
+}
+</script>
--- src/components/coordinate/Form.vue
+++ src/components/coordinate/Form.vue
@@ -95,61 +95,11 @@
                 </a-checkbox-group>
             </a-form-item>
 
-            <a-table
-                class="select-item-table"
-                :dataSource="tableItems"
-                :columns="columns"
-                :scroll="{ x: 1200, y: 500 }"
-                :pagination="{ pageSize: 50 }"
-                :loading="tableLoading"
-                :rowSelection="{
-                    selectedRowKeys,
-                    onSelect
-                }"
-            >
-                <span slot="genres" slot-scope="genres">
-                    <a-tag
-                        v-for="(genre, i) in genres"
-                        :color="genre.color"
-                        :key="i"
-                    >
-                        {{ genre.name }}
-                    </a-tag>
-                </span>
-
-                <span slot="price" slot-scope="price">
-                    <div class="column-price">
-                        {{ price.toLocaleString() }}
-                    </div>
-                </span>
-
-                <span slot="satisfaction" slot-scope="satisfaction">
-                    <a-rate
-                        :defaultValue="satisfaction"
-                        disabled
-                        allow-half
-                        style="text-align: center !important"
-                    />
-                </span>
-
-                <a-icon
-                    slot="filterIcon"
-                    slot-scope="filtered"
-                    type="search"
-                    :style="{ color: filtered ? '#108ee9' : undefined }"
-                />
-
-                <span slot="imageLink" slot-scope="imageLink">
-                    <img
-                        class="coordinate-item-table-image"
-                        :src="
-                            imageLink
-                                ? imageLink
-                                : require('@/assets/no-img.png')
-                        "
-                    />
-                </span>
-            </a-table>
+            <clothes-form-table
+                :tableItems="tableItems"
+                :selectedRowKeys="selectedRowKeys"
+                v-on:on-select="onSelect"
+            />
 
             <div class="form-submit-button">
                 <a-button html-type="submit" type="primary">
@@ -161,7 +111,7 @@
 </template>
 
 <script lang="ts">
-import { Vue, Component, Prop } from 'vue-property-decorator';
+import { Vue, Component, Prop, Emit } from 'vue-property-decorator';
 import * as Vuex from 'vuex';
 import { WrappedFormUtils } from 'ant-design-vue/types/form/form';
 import {
@@ -170,15 +120,16 @@ import {
 } from 'ant-design-vue/types/upload';
 
 import ClothesDetail from '@/components/clothes/Detail.vue';
+import ClothesFormTable from '@/components/clothes/FormTable.vue';
 import { Coordinate } from '@/store/coordinate/type';
 import { getBase64, isLt5M } from '@/util/file';
 import { formItemLayout } from './form';
-import { getColumns } from './table';
 import { FormFields, Season, Record } from './type';
 
 @Component({
     components: {
-        ClothesDetail
+        ClothesDetail,
+        ClothesFormTable
     }
 })
 export default class CoordinateForm extends Vue {
@@ -197,30 +148,7 @@ export default class CoordinateForm extends Vue {
 
     imageLoading = false;
 
-    tableLoading = false;
-
     selectedRowKeys: number[] = [];
-
-    columns = getColumns(
-        this.$store.getters['clothes/assistBrands'],
-        this.$store.getters['clothes/assistShops'],
-        this.$store.getters['clothes/assistGenres']
-    );
-
-    async beforeCreate() {
-        this.tableLoading = true;
-
-        await Promise.all([
-            this.$store.dispatch('coordinate/fetchCoordinateItems'),
-            this.$store.dispatch('clothes/fetchAssistGenres'),
-            this.$store.dispatch('clothes/fetchAssistBrands'),
-            this.$store.dispatch('clothes/fetchAssistShops')
-        ]).catch(err => {
-            this.$emit('on-error', err);
-        });
-
-        this.tableLoading = false;
-    }
 
     created() {
         const { target } = this;
@@ -281,8 +209,9 @@ export default class CoordinateForm extends Vue {
         })) as Record[];
     }
 
-    onSelect(record: Record) {
-        this.selectedRowKeys.push(record.key);
+    @Emit('on-select')
+    onSelect(key: number) {
+        this.selectedRowKeys.push(key);
         this.form.setFieldsValue({ clothingIds: this.selectedRowKeys });
     }
 
