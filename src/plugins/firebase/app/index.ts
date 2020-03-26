4bd041d7e8e1d23ecda3555e769df2120b85386b
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-26 13:23:25 +0900

    firebase におけるパフォーマンスチェックを追加

--- src/plugins/firebase/app/index.ts
+++ src/plugins/firebase/app/index.ts
@@ -1,4 +1,5 @@
-import firebase from 'firebase/app';
+import * as firebase from 'firebase/app';
+import 'firebase/performance';
 
 export default {
     init: () => {
@@ -8,7 +9,9 @@ export default {
             databaseURL: process.env.VUE_APP_FIREBASE_DATABASE_URL,
             projectId: process.env.VUE_APP_FIREBASE_PROJECT_ID,
             storageBucket: process.env.VUE_APP_FIREBASE_STORAGE_BUCKET,
-            messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID
+            messagingSenderId: process.env.VUE_APP_FIREBASE_MESSAGING_SENDER_ID,
+            appId: process.env.VUE_APP_FIREBASE_APP_ID
         });
+        firebase.performance();
     }
 };
