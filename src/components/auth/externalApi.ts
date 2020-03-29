e376a77a0b0ee0b53a98a559838d4a08f258b575
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-03-29 11:07:46 +0900

    message 関連の処理を１つのファイルにまとめるよう修正

--- src/components/auth/externalApi.ts
+++ src/components/auth/externalApi.ts
@@ -1,5 +1,3 @@
-import { FirebaseExternalApiAuthError } from 'firebase';
-import { AppMessage } from 'ant-design-vue/types/message';
 import VueRouter from 'vue-router';
 
 export const toHome = (again: string, router: VueRouter): void => {
@@ -8,15 +6,4 @@ export const toHome = (again: string, router: VueRouter): void => {
         return;
     }
     router.push({ name: 'home' });
-};
-
-export const mappingMessage = (
-    err: FirebaseExternalApiAuthError
-): AppMessage => {
-    return {
-        isShow: true,
-        text: `Error (${err.code})`,
-        description: `${err.message} ProviderId is "${err.credential.providerId}", Email is "${err.email}"`,
-        type: 'error'
-    };
 };
--- src/util/message.ts
+++ src/util/message.ts
@@ -1,4 +1,5 @@
 import { AppMessage } from 'ant-design-vue/types/message';
+import { FirebaseExternalApiAuthError } from 'firebase';
 
 export const resetMessage = (): AppMessage => ({
     isShow: false,
@@ -6,3 +7,21 @@ export const resetMessage = (): AppMessage => ({
     description: '',
     type: null
 });
+
+export const toErrorMessage = (err: any): AppMessage => ({
+    isShow: true,
+    text: `Error (${err.code})`,
+    description: `${err.message}`,
+    type: 'error'
+});
+
+export const toFirebaseErrorMessage = (
+    err: FirebaseExternalApiAuthError
+): AppMessage => {
+    return {
+        isShow: true,
+        text: `Error (${err.code})`,
+        description: `${err.message} ProviderId is "${err.credential.providerId}", Email is "${err.email}"`,
+        type: 'error'
+    };
+};
