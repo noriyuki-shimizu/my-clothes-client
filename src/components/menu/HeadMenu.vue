1b522d6d6bc2972d05c35bd1253811659cbe690b
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-04-19 11:04:26 +0900

    menu 部分をコンポーネント化

---
+++ src/components/menu/HeadMenu.vue
@@ -0,0 +1,34 @@
+<template>
+    <a-menu theme="dark" mode="horizontal" :style="{ lineHeight: '64px' }">
+        <a-sub-menu class="mobile-sub-menu">
+            <span slot="title" class="submenu-title-wrapper">
+                <a-icon type="menu" :style="{ fontSize: '20px' }" />
+            </span>
+            <a-menu-item>
+                Coordinate
+            </a-menu-item>
+            <a-menu-item-group title="Parts">
+                <a-menu-item key="clothes">Clothes</a-menu-item>
+                <a-menu-item key="brand">Brand</a-menu-item>
+                <a-menu-item key="genre">Genre</a-menu-item>
+                <a-menu-item key="shop">Shop</a-menu-item>
+            </a-menu-item-group>
+            <a-menu-item>
+                Price chart
+            </a-menu-item>
+        </a-sub-menu>
+    </a-menu>
+</template>
+
+<script>
+import { Component, Vue } from 'vue-property-decorator';
+
+@Component
+export default class HeadMenu extends Vue {}
+</script>
+
+<style scoped>
+.mobile-sub-menu {
+    width: 20vw;
+}
+</style>
--- src/views/mobile/Layout.vue
+++ src/views/mobile/Layout.vue
@@ -1,30 +1,7 @@
 <template>
     <a-layout id="components-layout-demo-top" class="layout">
         <a-layout-header class="mobile-header">
-            <div class="logo" />
-            <a-menu
-                theme="dark"
-                mode="horizontal"
-                :style="{ lineHeight: '64px' }"
-            >
-                <a-sub-menu class="mobile-sub-menu">
-                    <span slot="title" class="submenu-title-wrapper">
-                        <a-icon type="menu" :style="{ fontSize: '20px' }" />
-                    </span>
-                    <a-menu-item>
-                        Coordinate
-                    </a-menu-item>
-                    <a-menu-item-group title="Parts">
-                        <a-menu-item key="clothes">Clothes</a-menu-item>
-                        <a-menu-item key="brand">Brand</a-menu-item>
-                        <a-menu-item key="genre">Genre</a-menu-item>
-                        <a-menu-item key="shop">Shop</a-menu-item>
-                    </a-menu-item-group>
-                    <a-menu-item>
-                        Price chart
-                    </a-menu-item>
-                </a-sub-menu>
-            </a-menu>
+            <head-menu />
         </a-layout-header>
         <a-layout-content id="main_layout_content">
             <app-content id="content" />
@@ -40,32 +17,22 @@ import { Component, Vue } from 'vue-property-decorator';
 
 import AppContent from '@/components/layout/Content.vue';
 import AppFooter from '@/components/layout/Footer.vue';
+import HeadMenu from '@/components/menu/HeadMenu.vue';
 
 @Component({
     components: {
         AppFooter,
-        AppContent
+        AppContent,
+        HeadMenu
     }
 })
 export default class Layout extends Vue {}
 </script>
 
 <style scoped>
-#components-layout-demo-top-side-2 .logo {
-    width: 120px;
-    height: 31px;
-    background: rgba(255, 255, 255, 0.2);
-    margin: 16px 28px 16px 0;
-    float: left;
-}
-
 .mobile-header {
     padding: 0;
     text-align: right;
-}
-
-.mobile-sub-menu {
-    width: 20vw;
 }
 
 #main_layout_content {
