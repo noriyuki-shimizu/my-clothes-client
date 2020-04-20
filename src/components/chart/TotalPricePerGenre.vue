5098b5f1dca1a7b15618d66274d860a9099a3751
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-04-20 22:12:22 +0900

    ジャンルごとの合計金額チャートコンポーネントを追加

---
+++ src/components/chart/TotalPricePerGenre.vue
@@ -0,0 +1,69 @@
+<template>
+    <a-empty class="empty-data" v-if="!totalPricePerGenres.length" />
+    <pie-chart v-else :chartData="chartData" :chartOptions="chartOptions" />
+</template>
+
+<script lang="ts">
+import { Component, Vue, Prop } from 'vue-property-decorator';
+import * as Vuex from 'vuex';
+import PieChart from '@/components/chart/PieChart';
+
+type ChartDataParts = {
+    labels: string[];
+    backgroundColor: string[];
+    data: number[];
+};
+
+@Component({
+    components: {
+        PieChart
+    }
+})
+export default class TotalPricePerGenre extends Vue {
+    @Prop({ type: Object as () => Chart.ChartOptions, required: true })
+    chartOptions!: Chart.ChartOptions;
+
+    $store!: Vuex.ExStore;
+
+    created() {
+        this.$store
+            .dispatch('genre/fetchTotalPricePerGenres')
+            .catch((err: any) => this.$emit('on-error', err));
+    }
+
+    get totalPricePerGenres() {
+        return this.$store.getters['genre/totalPricePerGenres'];
+    }
+
+    get chartData() {
+        const {
+            labels,
+            backgroundColor,
+            data
+        } = this.totalPricePerGenres.reduce(
+            (accumulator: ChartDataParts, currentValue) => {
+                accumulator.labels.push(currentValue.name);
+                accumulator.backgroundColor.push(currentValue.color);
+                accumulator.data.push(currentValue.totalPrice);
+                return accumulator;
+            },
+            { labels: [], backgroundColor: [], data: [] }
+        );
+        return {
+            labels,
+            datasets: [
+                {
+                    backgroundColor,
+                    data
+                }
+            ]
+        };
+    }
+}
+</script>
+
+<style>
+.empty-data {
+    padding: 24vh 0;
+}
+</style>
