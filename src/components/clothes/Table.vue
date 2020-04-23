9880884ddcaa54e72a0cf32224898e854268e75e
清水紀行 <noriyuki.shimizu0612@gmail.com>
2020-04-23 20:16:21 +0900

    親コンポーネントの値はそのコンポーネントで変更するよう変更

--- src/components/clothes/Table.vue
+++ src/components/clothes/Table.vue
@@ -145,24 +145,24 @@ export default class ClothesTable extends Vue {
 
     async onDelete(id: number) {
         this.$emit('on-reset-message');
-        this.loading = true;
+        this.$emit<boolean>('set-loading', true);
 
         await this.$store
             .dispatch('clothes/onDeleteClothes', id)
             .catch((err: any) => this.$emit('on-error', err));
 
-        this.loading = false;
+        this.$emit<boolean>('set-loading', false);
     }
 
     async onRestoration(id: number) {
         this.$emit('on-reset-message');
-        this.loading = true;
+        this.$emit<boolean>('set-loading', true);
 
         await this.$store
             .dispatch('clothes/onRestorationClothes', id)
             .catch((err: any) => this.$emit('on-error', err));
 
-        this.loading = false;
+        this.$emit<boolean>('set-loading', false);
     }
 }
 </script>
--- src/views/web/parts/clothes/Index.vue
+++ src/views/web/parts/clothes/Index.vue
@@ -32,6 +32,7 @@
             :loading="loading"
             v-on:on-error="onError"
             v-on:on-reset-message="onResetMessage"
+            v-on:set-loading="setLoading"
         />
     </div>
 </template>
@@ -111,6 +112,11 @@ export default class Shop extends Vue {
     @Emit('on-reset-message')
     onResetMessage() {
         this.message = resetMessage();
+    }
+
+    @Emit('set-loading')
+    setLoading(loading: boolean) {
+        this.loading = loading;
     }
 
     @Emit('on-error')
